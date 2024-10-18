f885793e87681544dff7c8e36c93355cee7ee237
Cosmin <cosmin072@gmail.com>
2024-10-18 15:32:56 +0300

    initial get route play

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
 import express from "express";
 
-PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+app.get("/json", (req, res) => {
+  res.send({ message: "Hello World!" });
+});
+
+const users = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+  { id: 3, name: "Charlie" },
+];
+
+app.get("/api/users", (req, res) => {
+  res.send(users);
+});
+app.get("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  const parsedId = parseInt(id);
+  console.log(parsedId);
+  if (isNaN(parsedId)) {
+    return res.status(400).send({ message: "Invalid ID" });
+  }
+  const findUser = users.find((user) => user.id === parsedId);
+  if (!findUser) return res.sendStatus(404);
+  return res.send(findUser);
 });
 
 app.listen(PORT, () => {
